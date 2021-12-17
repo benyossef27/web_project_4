5bdf5ea94255f6b16b97de7205c27ff73a3f7043
benyossef27 <93250111+benyossef27@users.noreply.github.com>
2021-12-17 12:24:00 +0200

    add popupjs open&close

--- blocks/popup/popup.js
+++ blocks/popup/popup.js
@@ -2,19 +2,22 @@
 let popup = document.querySelector("#popup");
 let popupButton = document.querySelector("#popup-button");
 
-/*opening the popup window*/
-function display(){
-    popup.style.display = "block";
-}    
+//opening the popup window
+function display() {
+    popup.classList.remove("popup");
+    popup.classList.add("popup_open");
+}
+//calling the button
+popupButton.addEventListener("click", display)
 
-popupButton.addEventListener("click", display);
-
-/*choosing the close button*/
-let popupClose = document.querySelector("#popup__close");
+//choosing the close button*/
+let popupClose = document.querySelector(".popup__close");
 
 /*closing the popup window without saving*/
 function close() {
-    popup.style.display = "none";
-}
+    popup.classList.remove("popup_open");
+    popup.classList.add("popup");
 
+}
+//calling the button
 popupClose.addEventListener("click", close);
