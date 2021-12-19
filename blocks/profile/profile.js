d7d8d7dffc5566792223a0fa0a527195a52c4e7c
benyossef27 <93250111+benyossef27@users.noreply.github.com>
2021-12-19 16:16:58 +0200

    fix popup form position

--- blocks/profile/profile.js
+++ blocks/profile/profile.js
@@ -1,26 +1,23 @@
 // Let's find the form in the DOM
-let formElement = document.querySelector(".profile__detailes");
-
+let formElement = document.querySelectorAll(".popup__input");
 
 function handleProfileFormSubmit(evt) {
   evt.preventDefault();
     // Let's find the form fields in the DOM
-    let nameInput = document.querySelector(".profile__name");// Use querySelector()
-    let jobInput = document.querySelector(".profile__job");// Use querySelector()
+    let nameInput = document.querySelector(".popup__name");
+    let jobInput = document.querySelector(".popup__job");
 
     // Get the values of each field from the corresponding value property
-    getval(nameInput) = nameEnter;
-    getval(jobInput) = jobEnter;
+    
     // Select elements where the field values will be entered
-let nameEnter = document.querySelector(".popup__name");
-let jobEnter = document.querySelector(".popup__job");
+    let nameEnter = document.querySelector(".profile__name");
+    let jobEnter = document.querySelector(".profile__job");
     // Insert new values using the textContent 
-    nameInput.textContent =  nameEnter;
-    jobInput.textContent = jobEnter;    
+    nameEnter.textContent = nameInput.value;
+    jobEnter.textContent = jobInput.value;
     // property of the querySelector() method
-    document.querySelectorAll(".popup__input").submit();
+    document.querySelector(".popup__input").submit();
 }
-
 // Connect the handler to the form:
 // it will watch the submit event
 formElement.addEventListener('submit', handleProfileFormSubmit);
