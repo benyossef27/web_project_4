15606c17e00bc230bcf7c63177c302f484fcebb2
benyossef27 <93250111+benyossef27@users.noreply.github.com>
2021-12-20 19:18:06 +0200

    new update for my project

--- blocks/popup/popup.js
+++ blocks/popup/popup.js
@@ -1,36 +1,36 @@
-/*open close realted*/
+//popup open & close realted
 const popup = document.querySelector(".popup");
 const popupButton = document.querySelector(".profile__popup-button");
 const popupOpen = document.querySelector(".popup__open");
 const popupClose = document.querySelector(".popup__close");
 
-//text content related
+//text content submition related
 const profileName = document.querySelector(".profile__name");
 const profileJob = document.querySelector(".profile__job");
 const submitButten = document.querySelector(".popup__save");
 
-//forms
+//forms 
 const profileForm = document.forms.profile;
 const profileFormNameInput = profileForm.elements.name;
 const profileFormJobInput = profileForm.elements.job;
 
-//opening the popup window
+//opening popup window
 function display() {
     popup.classList.remove("popup");
     popup.classList.add("popup_open");
 }
-//calling the button
+//listener open button
 popupButton.addEventListener("click", display);
 
-/*closing the popup window without saving*/
+//closing popup window without submit
 function close() {
     popup.classList.remove("popup_open");
     popup.classList.add("popup");
 }
-//calling the button
+//listener close button
 popupClose.addEventListener("click", close);
 
- //submitting the form and closing it
+//submitting & closing form
 profileForm.addEventListener("submit", function(event){
     profileName.textContent = profileFormNameInput.value;
     profileJob.textContent = profileFormJobInput.value;
