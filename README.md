1b713bd4d82f4bcd052ed42e74abdffdba4110a7
benyossef27 <93250111+benyossef27@users.noreply.github.com>
2022-01-03 16:31:28 +0200

    went back

--- README.md
+++ README.md
@@ -11,13 +11,11 @@
 
 This is a web project showcasing the profile of a traveller around the US and a gallery of places visited.
 
-Users may change the details of the profile user to thier own.
-
-Users may add, like or remove cards with places they visited.
+Users may change the details of the profile user to thier own. 
 
 ## coming up next 
 
-More interactivity and some backend properties.
+Users will be able to add a name and image of places they visited to the web page allowing others to like them.
 
 ## Technology stack, techniques and tools used
 
@@ -28,7 +26,7 @@ More interactivity and some backend properties.
 
 ## Figma
 
-[Link to the project design in Figma](https://www.figma.com/file/m79HxYeZpOXRw0Tz2eZGOV/Sprint-5-Around-The-U.S.-desktop-mobile?node-id=0%3A1)
+[Link to the project design in Figma](https://www.figma.com/file/SurN1jaeEQIhuZEDMhmWWf/Sprint-4%3A-Around-The-U.S.-%7C-desktop-%2B-mobile?node-id=0%3A1)
 
 ## Project Demo Link
 
--- blocks/popup/popup.js
+++ blocks/popup/popup.js
@@ -8,33 +8,34 @@ const profileName = document.querySelector(".profile__name");
 const profileJob = document.querySelector(".profile__job");
 const submitButten = document.querySelector(".popup__save");
 
-//forms
+//forms 
 const profileForm = document.forms.profile;
 const profileFormNameInput = profileForm.elements.name;
 const profileFormJobInput = profileForm.elements.job;
 
 //opening popup window
 function display() {
-  popup.classList.add("popup_open");
-  profileFormNameInput.value = profileName.textContent;
-  profileFormJobInput.value = profileJob.textContent;
-}
+    popup.classList.add("popup_open");
+    profileFormNameInput.value = profileName.textContent;
+    profileFormJobInput.value = profileJob.textContent;
+}   
 //listener open button
 popupButton.addEventListener("click", display);
 
 //closing popup window without submit
 function close() {
-  popup.classList.remove("popup_open");
+    popup.classList.remove("popup_open");
 }
 //listener close button
 popupClose.addEventListener("click", close);
 
 //submitting & closing form
 function submit(event) {
-  profileName.textContent = profileFormNameInput.value;
-  profileJob.textContent = profileFormJobInput.value;
-  event.preventDefault();
-  close();
+    profileName.textContent = profileFormNameInput.value;
+    profileJob.textContent = profileFormJobInput.value;
+    event.preventDefault();
+    close();
 }
 //listner submit butten
 profileForm.addEventListener("submit", submit);
+
