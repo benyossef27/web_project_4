2a419b60ab776dfa0bd37c2b097ff76623e0a913
benyossef27 <93250111+benyossef27@users.noreply.github.com>
2022-03-15 15:12:13 +0200

    ready#1

--- src/pages/index.js
+++ src/pages/index.js
@@ -22,7 +22,6 @@ import {
   profileAvatar,
   avatarImageInput,
   popupCardDelete,
-  popupUserAvatarForm,
 } from "../utils/constants.js";
 import Section from "../components/Section.js";
 import UserInfo from "../components/UserInfo.js";
