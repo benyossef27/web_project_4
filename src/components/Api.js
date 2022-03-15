a9e6150a060511568c22844b0c6d835af47fde67
benyossef27 <93250111+benyossef27@users.noreply.github.com>
2022-03-15 15:11:15 +0200

    Patch Api

--- src/components/Api.js
+++ src/components/Api.js
@@ -3,16 +3,20 @@ class Api {
     this._baseUrl = baseUrl;
     this._headers = headers;
   }
+
+  _customFetch = (url, headers) => {
+    return fetch(url, headers).then((res) =>
+      res.ok ? res.json() : Promise.reject(`Error: ${res.status}`)
+    );
+  };
   ///////////////////user////////////////////
   getUserInfo() {
-    return fetch(`${this._baseUrl}/users/me `, {
+    return this._customFetch(`${this._baseUrl}/users/me`, {
       headers: this._headers,
-    })
-      .then((res) => (res.ok ? res.json() : Promise.reject(res.statusText)))
-      .catch((err) => console.log(err));
+    });
   }
   setUserInfo({ name, about }) {
-    return fetch(`${this._baseUrl}/users/me `, {
+    return this._customFetch(`${this._baseUrl}/users/me `, {
       headers: this._headers,
       method: "PATCH",
 
@@ -20,68 +24,46 @@ class Api {
         name,
         about,
       }),
-    })
-      .then((res) => (res.ok ? res.json() : Promise.reject(res.statusText)))
-      .catch((err) => console.log(err));
+    });
   }
   setUserAvatar(data) {
-    return fetch(`${this._baseUrl}/users/me/avatar`, {
+    return this._customFetch(`${this._baseUrl}/users/me/avatar`, {
       headers: this._headers,
       method: "PATCH",
       body: JSON.stringify({ avatar: data }),
-    })
-      .then((res) => (res.ok ? res.json() : Promise.reject(res.statusText)))
-      .catch((err) => console.log(err));
+    });
   }
   ////////////////////////cards//////////////////////////////////
   getInitialCards() {
-    return fetch(`${this._baseUrl}/cards`, {
+    return this._customFetch(`${this._baseUrl}/cards`, {
       headers: this._headers,
-    })
-      .then((res) => (res.ok ? res.json() : Promise.reject(res.statusText)))
-      .catch((err) => console.log(err));
+    });
   }
   createCard(item) {
-    return fetch(`${this._baseUrl}/cards `, {
+    return this._customFetch(`${this._baseUrl}/cards `, {
       headers: this._headers,
       method: "POST",
       body: JSON.stringify(item),
-    })
-      .then((res) => (res.ok ? res.json() : Promise.reject(res.statusText)))
-      .catch((err) => {
-        console.log(err);
-      });
+    });
   }
 
   deleteCard(id) {
-    return fetch(`${this._baseUrl}/cards/${id} `, {
+    return this._customFetch(`${this._baseUrl}/cards/${id} `, {
       headers: this._headers,
       method: "DELETE",
-    })
-      .then((res) => (res.ok ? res.json() : Promise.reject(res.statusText)))
-      .catch((err) => {
-        console.log(err);
-      });
+    });
   }
   likeCard(id) {
-    return fetch(`${this._baseUrl}/cards/likes/${id}`, {
+    return this._customFetch(`${this._baseUrl}/cards/likes/${id}`, {
       headers: this._headers,
       method: "PUT",
-    })
-      .then((res) => (res.ok ? res.json() : Promise.reject(res.statusText)))
-      .catch((err) => {
-        console.log(err);
-      });
+    });
   }
   unlikeCard(id) {
-    return fetch(`${this._baseUrl}/cards/likes/${id}`, {
+    return this._customFetch(`${this._baseUrl}/cards/likes/${id}`, {
       headers: this._headers,
       method: "DELETE",
-    })
-      .then((res) => (res.ok ? res.json() : Promise.reject(res.statusText)))
-      .catch((err) => {
-        console.log(err);
-      });
+    });
   }
 }
 
